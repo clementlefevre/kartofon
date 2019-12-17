e5ad374df4c521697e966e36e5a813a7e3cc42b4
clementlefevre <clement.san@gmail.com>
2019-12-17 13:58:20 +0000

    Add README

--- devtest.py
+++ devtest.py
@@ -5,7 +5,9 @@ import time
 from evdev import InputDevice, categorize, ecodes
 from select import select
 
-device = InputDevice('/dev/input/event1')
+#device = InputDevice('/dev/input/event1')
+device = InputDevice('/dev/input/by-id/usb-13ba_Barcode_Reader-event-kbd')
+
 print(device)
 
 keys = "X^1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX"
@@ -13,13 +15,16 @@ keys = "X^1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXX
 TITLES={}
 
 def read_titles():
-    f = open('/home/pi/kartofon/config.txt', 'r')
+    f = open('/home/pi/workspace/kartofon/config.txt', 'r')
     TITLES = {}
     for line in f:
         line = line.replace('\n','')
         if len(line)>0:
             k, v = line.strip().split(' ')
-            TITLES[k.strip()] = v.strip()
+            if k.strip() not in TITLES :
+                TITLES[k.strip()] = [v.strip()]
+            else :
+                TITLES[k.strip()].append(v.strip())
 
     f.close()
     return TITLES
@@ -33,16 +38,18 @@ player = instance.media_player_new()
 
 #Load the media file
 def play_file(player, filename):
-    filename = ''.join(['/home/pi/kartofon/media/',filename,'.mp3'])
+    filename = ''.join(['/home/pi/workspace/kartofon/media/',filename,'.mp3'])
     media = instance.media_new(filename)
-
+    print(f'file to play {filename}')
 #Add the media to the player
     player.set_media(media)
 
-#Play for 10 seconds then exit
+
     print("start playing", filename)
     player.play()
-    
+    time.sleep(1.5)
+    duration = player.get_length() / 1000
+    time.sleep(duration) 
 
 def read_rfid(device):
 	stri=''
@@ -58,7 +65,7 @@ def read_rfid(device):
 	return(stri[:-1])
 
 TITLES = read_titles()
-
+print(f'Titles : {TITLES}')
 
 while True :
     RFID = read_rfid(device)
@@ -66,5 +73,6 @@ while True :
         player.stop()
     if (RFID in TITLES.keys()):
         print(TITLES[RFID])
-        play_file(player, TITLES[RFID])
+        for file in TITLES[RFID]:
+            play_file(player, file)
 
--- kartofon.sh
+++ kartofon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(sleep 30;amixer set Master 100%;/usr/bin/python /home/pi/kartofon/devtest.py &>>/home/pi/kartofon/kartofon.log)
+(sleep 30;amixer set Master 100%;/home/pi/berryconda3/bin/python /home/pi/workspace/kartofon/devtest.py &>>/home/pi/workspace/kartofon/kartofon.log)
